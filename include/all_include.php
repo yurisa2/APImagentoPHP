3dd9576cb91ee6a80a33fea35fa32cb35a46ac1d
yurisa2 <yuri@sa2.com.br>
2016-09-30 11:33:44 -0300

    Isset Prefix_m to squash a notice on php

--- include/all_include.php
+++ include/all_include.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!isset($prefix_m)) $prefix_m = '';
+
 require_once $prefix_m.'include/config.php';  //Fixed data (users, passwds, urls)
 require_once $prefix_m.'include/basic.php';   //Object, info etc etc
 require_once $prefix_m.'include/read.php';    //Functions for reading from DB
