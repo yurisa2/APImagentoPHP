bfc9af5df88f1e7f24f7b57970d15e6bc117cbee
Alberto Aguiar <alberto.aguiar@fatec.sp.gov.br>
2016-10-13 23:32:57 -0300

    function update product

--- include/update.php
+++ include/update.php
@@ -1,18 +1,29 @@
 <?php
 function magento_catalogInventoryStockItemUpdate($sku,$qty)
 {
-$mod = array(
- 'qty' => $qty
+	$mod = array(
+		'qty' => $qty
+	);
 
+	$return = magento_obj()->catalogInventoryStockItemUpdate(magento_session(),$sku,$mod);
 
-);
+	return $return;
+}
 
+function magento_catalogProductUpdate($sku,$mod)
+{
+	$mod = array(
+		'name' => "sa2 consultoria",
+		'description' => "Aqui é a descrição do produto",
+		'short_description' => "Aqui é a descrição curta do produto",
+		'weight' => "10",
+		'status' => "1",
+		'price' => "10"
+	);
 
+	$return = magento_obj()->catalogProductUpdate(magento_session(),$sku,$mod);
 
-
-  $return = magento_obj()->catalogInventoryStockItemUpdate(magento_session(),$sku,$mod);
-
-  return $return;
+	return $return;
 }
 
 ?>
