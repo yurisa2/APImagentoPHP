37870c99ed012b3b62ff8ef4d1ae110a1de6187e
Alberto Aguiar <alberto.aguiar@fatec.sp.gov.br>
2016-11-01 11:39:47 -0200

    function update exclused

--- include/update.php
+++ include/update.php
@@ -10,22 +10,4 @@ function magento_catalogInventoryStockItemUpdate($sku,$qty)
 	return $return;
 }
 
-function magento_catalogProductUpdate($sku,$mod)
-{
-	$data_product_mercosistem = write_product_mercosistem_magento($sku);
-
-	$mod = array(
-		'name' => $data_product_mercosistem['name'],
-		'description' => $data_product_mercosistem['description'],
-		'short_description' => $data_product_mercosistem['short_description'],
-		'weight' => $data_product_mercosistem['weight'],
-		'price' => $data_product_mercosistem['price'],
-		'qty_in_stock' => $data_product_mercosistem['qty_in_stock']
-	);
-
-	$return = magento_obj()->catalogProductUpdate(magento_session(),$sku,$mod);
-
-	return $return;
-}
-
 ?>
