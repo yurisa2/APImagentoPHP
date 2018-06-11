6d7d1cbe20d120fa4adc1b40e72e1497a1874acf
yurisa2 <yuri@sa2.com.br>
2018-06-11 16:15:11 -0300

    Create inclusion

--- include/all_include.php
+++ include/all_include.php
@@ -6,5 +6,6 @@ require_once $prefix_m.'include/config.php';  //Fixed data (users, passwds, urls
 require_once $prefix_m.'include/basic.php';   //Object, info etc etc
 require_once $prefix_m.'include/read.php';    //Functions for reading from DB
 require_once $prefix_m.'include/update.php';  //Functioons for updating db
+require_once $prefix_m.'include/create.php';  //Functioons for updating db
 
 ?>
