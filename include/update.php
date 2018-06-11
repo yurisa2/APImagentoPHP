7e4b7591b05771b5cf42cc022f0f1b1b8e57cdbc
yurisa2 <yuri@sa2.com.br>
2018-06-11 16:12:14 -0300

    Cart update

--- include/update.php
+++ include/update.php
@@ -2,13 +2,13 @@
 function magento_catalogInventoryStockItemUpdate($sku,$qty)
 {
 	global $magento_soap_user;
-  global $magento_soap_password;
+	global $magento_soap_password;
 
-  $obj_magento = magento_obj();
-  $session = magento_session();
+	$obj_magento = magento_obj();
+	$session = magento_session();
 
-  $is_in_stock = 1;
-  if($qty <= 0) $is_in_stock = 0;
+	$is_in_stock = 1;
+	if($qty <= 0) $is_in_stock = 0;
 
 	$mod = array(
 		'qty' => $qty,
@@ -23,36 +23,36 @@ function magento_catalogInventoryStockItemUpdate($sku,$qty)
 function magento_catalogProductUpdate($sku,$mod)
 {
 	global $magento_soap_user;
-  global $magento_soap_password;
+	global $magento_soap_password;
 
-  $obj_magento = magento_obj();
-  $session = magento_session();
+	$obj_magento = magento_obj();
+	$session = magento_session();
 
 	/* $mod Array template
 	$mod = array(
-		'name' => $data_product['name'],
-		'description' => $data_product['description'],
-		'short_description' => $data_product['short_description'],
-		'weight' => $data_product['weight'],
-		'price' => $data_product['price']
-	);
-	*/
+	'name' => $data_product['name'],
+	'description' => $data_product['description'],
+	'short_description' => $data_product['short_description'],
+	'weight' => $data_product['weight'],
+	'price' => $data_product['price']
+);
+*/
 
-	$return = $obj_magento->catalogProductUpdate($session,$sku,$mod);
+$return = $obj_magento->catalogProductUpdate($session,$sku,$mod);
 
-	return $return;
+return $return;
 }
 
 function magento_catalogProductUpdate_and_stock($sku,$mod)
 {
 	global $magento_soap_user;
- 	global $magento_soap_password;
+	global $magento_soap_password;
 
 	$obj_magento = magento_obj();
 	$session = magento_session();
 
-/* $mod Array template
-$mod = array(
+	/* $mod Array template
+	$mod = array(
 	'name' => $data_product['name'],
 	'description' => $data_product['description'],
 	'short_description' => $data_product['short_description'],
@@ -62,71 +62,71 @@ $mod = array(
 );
 */
 
-	if($mod) {
-		$mod_update = [];
-		$mod_update_return = [];
+if($mod) {
+	$mod_update = [];
+	$mod_update_return = [];
 
-		if(!empty($mod['name'])) {
-			$mod_update['name'] = $mod['name'];
-			$mod_update_return['name'] = $mod['name'];
-		}
-
-		if(!empty($mod['description'])) {
-			$mod_update['description'] = $mod['description'];
-			$mod_update_return['description'] = $mod['description'];
-		}
-
-		if(!empty($mod['short_description'])) {
-			$mod_update['short_description'] = $mod['short_description'];
-			$mod_update_return['short_description'] = $mod['short_description'];
-		}
-
-		if(!empty($mod['weight'])) {
-			$mod_update['weight'] = $mod['weight'];
-			$mod_update_return['weight'] = $mod['weight'];
-		}
-
-		if(!empty($mod['price'])) {
-			$mod_update['price'] = $mod['price'];
-			$mod_update_return['price'] = $mod['price'];
-		}
-
-		$update_product = $obj_magento->catalogProductUpdate($session,$sku,$mod_update);
-
-		$mod_update['tier_price'] = $mod['tier_price'];
-		$mod_update_return['tier_price'] = $mod['tier_price'];
-
-		$update_tier_price = $obj_magento->catalogProductAttributeTierPriceUpdate($session,$sku, $mod_update['tier_price']);
-
-		if(!empty($mod['qty_in_stock'])) {
-			$qty = $mod['qty_in_stock'];
-			$is_in_stock = 1;
-  			if($qty <= 0) $is_in_stock = 0;
-
-			$mod_qty = array(
-				'qty' => $qty,
-				'is_in_stock' => $is_in_stock
-			);
-			$mod_update_return['qty_in_stock'] = $mod['qty_in_stock'];
-			$obj_magento->catalogInventoryStockItemUpdate($session,$sku,$mod_qty);
-		}
+	if(!empty($mod['name'])) {
+		$mod_update['name'] = $mod['name'];
+		$mod_update_return['name'] = $mod['name'];
 	}
 
-
-	if(isset($update_product)) {
-		$return = $mod_update_return;
-	} else {
-		$return = false;
+	if(!empty($mod['description'])) {
+		$mod_update['description'] = $mod['description'];
+		$mod_update_return['description'] = $mod['description'];
 	}
 
-	return $return;
+	if(!empty($mod['short_description'])) {
+		$mod_update['short_description'] = $mod['short_description'];
+		$mod_update_return['short_description'] = $mod['short_description'];
+	}
+
+	if(!empty($mod['weight'])) {
+		$mod_update['weight'] = $mod['weight'];
+		$mod_update_return['weight'] = $mod['weight'];
+	}
+
+	if(!empty($mod['price'])) {
+		$mod_update['price'] = $mod['price'];
+		$mod_update_return['price'] = $mod['price'];
+	}
+
+	$update_product = $obj_magento->catalogProductUpdate($session,$sku,$mod_update);
+
+	$mod_update['tier_price'] = $mod['tier_price'];
+	$mod_update_return['tier_price'] = $mod['tier_price'];
+
+	$update_tier_price = $obj_magento->catalogProductAttributeTierPriceUpdate($session,$sku, $mod_update['tier_price']);
+
+	if(!empty($mod['qty_in_stock'])) {
+		$qty = $mod['qty_in_stock'];
+		$is_in_stock = 1;
+		if($qty <= 0) $is_in_stock = 0;
+
+		$mod_qty = array(
+			'qty' => $qty,
+			'is_in_stock' => $is_in_stock
+		);
+		$mod_update_return['qty_in_stock'] = $mod['qty_in_stock'];
+		$obj_magento->catalogInventoryStockItemUpdate($session,$sku,$mod_qty);
+	}
+}
+
+
+if(isset($update_product)) {
+	$return = $mod_update_return;
+} else {
+	$return = false;
+}
+
+return $return;
 }
 
 //changing order status
 function magento_salesOrderAddComment($mod)
 {
 	global $magento_soap_user;
- 	global $magento_soap_password;
+	global $magento_soap_password;
 
 	$obj_magento = magento_obj();
 	$session = magento_session();
@@ -138,6 +138,68 @@ function magento_salesOrderAddComment($mod)
 
 
 	$return = $obj_magento->salesOrderAddComment($session, $orderIncrementId, $status, $comment, $notify);
+
+	return $return;
+}
+
+function magento_shoppingCartProductAdd($cart_id,$prods)
+{
+	//$prods Array template
+	// array(
+	// 	array($sku,$qty),
+	// 	array($sku,$qty)
+	// )
+
+	global $magento_soap_user;
+	global $magento_soap_password;
+
+	$return = 0;
+
+	$obj_magento = magento_obj();
+	$session = magento_session();
+
+	foreach ($prods as $key => $value) {
+		$sku_ = $value[0]; //very very very wrong...but...
+		$qty_ = $value[1];	//If I were to use another array, safer
+
+
+		$shoppingCartProductEntity[] =
+		array(
+			'sku' => $sku_,
+			'qty' => $qty_
+		);
+		// var_dump($shoppingCartProductEntity); //DEBUG
+
+	}
+	$result_prod_add = $obj_magento->shoppingCartProductAdd($session,$cart_id,$shoppingCartProductEntity);
+	if($result_prod_add) $return++;
+	// var_dump($return);
+
+	return $return;
+}
+
+function magento_shoppingCartCustomerSet($ord_id,$cust)
+{
+	global $magento_soap_user;
+	global $magento_soap_password;
+
+	$obj_magento = magento_obj();
+	$session = magento_session();
+
+	$return = $obj_magento->shoppingCartCustomerSet($session, $ord_id, $cust);
+
+	return $return;
+}
+
+function magento_shoppingCartCustomerAddresses($ord_id,$billing)
+{
+	global $magento_soap_user;
+	global $magento_soap_password;
+
+	$obj_magento = magento_obj();
+	$session = magento_session();
+
+	$return = $obj_magento->shoppingCartCustomerAddresses($session, $ord_id, $billing);
 
 	return $return;
 }
