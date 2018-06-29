b827b310388a150d0a86ba9bfc7f7e99a947145f
luigimuzy <luigifracalanza@gmail.com>
2018-06-29 16:26:08 -0300

    TEntando criar order no magento com o obj criado a partir do ML

--- index.php
+++ index.php
@@ -54,9 +54,9 @@ $dadosVenda->numero_documento_comprador = "123456789";
 
 $DEBUG =TRUE;
 
-$teste = new Magento_order($dadosVenda);
+//$teste = new Magento_order($dadosVenda);
 
-var_dump($teste);
+//var_dump($teste);
 
 // magento_info();
 
@@ -77,161 +77,162 @@ var_dump($teste);
 // var_dump(magento_shoppingCartCreate());
 
 
-// $prods = array(
-//   array('EP-51-40983','1'),
-//   array('EP-51-40654','4')
-// );
-//
-// $billing =
-// array(
-//   array(
-//     'mode' => 'billing',
-//     'firstname' => 'Teste Magento',
-//     'lastname' => 'Teste Magento',
-//     'street' => 'street address',
-//     'city' => 'Ficticio de Teste',
-//     'region' => 'SP',
-//     'postcode' => 'postcode',
-//     'country_id' => 'BR',
-//     'telephone' => '34343434343434',
-//     'is_default_billing' => 0,
-//     'is_default_shipping' => 0),
-//   array(
-//     'mode' => 'shipping',
-//     'firstname' => 'Teste Magento',
-//     'lastname' => 'Teste Magento',
-//     'street' => 'street address',
-//     'city' => 'Ficticio de Teste',
-//     'region' => 'SP',
-//     'postcode' => 'postcode',
-//     'country_id' => 'BR',
-//     'telephone' => '34343434343434',
-//     'is_default_billing' => 0,
-//     'is_default_shipping' => 0)
-//   );
-//
-// // $customerCreate = array(
-// //   'email' => 'cust-tst-mail@example.org',
-// //   'firstname' => 'Dough',
-// //   'lastname' => 'Deeks',
-// //   'password' => 'password',
-// //   'website_id' => 1,
-// //   'store_id' => 1,
-// //   'group_id' => 1);
-// //
-// // var_dump(magento_customerCustomerCreate($customerCreate));
-//
-// // $cust = array(
-// //   //concatenar o email no nome
-// //     'email' => 'customer-mail@example.org',
-// //     'firstname' => "testFirstname",
-// //     'lastname' => "testLastName",
-// //     'mode' => "guest",
-// //     'group_id' => "1"
-// //     );
-//
-// $customer = array(
-//   'firstname' => rand(1,100)."testFirstname",
-//   'lastname' => "testLastName1",
-//   'email' => rand(1,100)."testmailcustomer1@mail.com",
-//   'telephone' => rand(1,100)."0001234567",
-//   'taxvat' => rand(100000, 150000),
-//   'group_id' => "1",
-//   'store_id' => "21",
-//   'website_id' => "2"
-// );
-//
-// $id_customer = magento_customerCustomerCreate($customer);
-//
-// $session = magento_session();
-// $obj_magento = magento_obj();
-// $obj_magento->customerAddressCreate($session, 34, array(
-// 'firstname' => rand(1,100).'teste1',
-// 'lastname' => 'testando',
-// 'street' => array('Street line 1', 'Streer line 2'),
-// 'city' => 'teste',
-// 'country_id' => 'BR',
-// 'region' => 'SP',
-// 'postcode' => rand(1,100).'96093',
-// 'telephone' => rand(1,100).'-623-2513',
-// 'is_default_billing' => FALSE,
-// 'is_default_shipping' => FALSE));
-//
-// //var_dump($obj_magento);
-//
-//
-// $dados_customer = magento_customerCustomerList($id_customer);
-// var_dump($dados_customer);
-//
-//
-// $cust = array(
-//     'customer_id' => $id_customer,
-//     'mode' => "customer"
-//    );
-//
-// $payment = array(
-//     'po_number' => null,
-//     'method' => 'cashondelivery',
-//     'cc_cid' => null,
-//     'cc_owner' => null,
-//     'cc_number' => null,
-//     'cc_type' => null,
-//     'cc_exp_year' => null,
-//     'cc_exp_month' => null
-//     );
-//
-//   // var_dump($prods);
-//
-//
-//
-//   $store_id = '21';
-//
-//   $carrinho = magento_shoppingCartCreate($store_id);
-//
-// $id = magento_session();
-//
-// // $obj_cust = magento_customerCustomerList($id);
-// // $obj_cust = (array) $obj_cust;
-//
-// // var_dump($obj_cust);
-// //
-// // var_dump(magento_obj()->customerGroupList($id));
-// //
-//
-//   echo "Retorno cartProdAdd: <br>";
-//   var_dump(magento_shoppingCartProductAdd($carrinho,$prods,$store_id));
-//   echo "<br>";
-//
-//   echo "Lista shoppingCartProductList: <br>";
-//   $lista_carrinho = magento_shoppingCartProductList($carrinho, $store_id);
-//   var_dump($lista_carrinho);
-//
-//   echo "# de itens shoppingCartProductList: ".count($lista_carrinho)." <br>";
-//   var_dump($cust);
-//   echo "Customer Set: ".magento_shoppingCartCustomerSet($carrinho,$cust ,$store_id)." <br>";
-//
-//   var_dump($billing);
-//   echo "Billing Set: ".magento_shoppingCartCustomerAddresses($carrinho, $billing,$store_id)." <br>";
-//
-//
-//
-//       // echo "<h1>INFORMAÇÕES DA LOJA-PAYMENT:</h1> <BR>";
-//       // var_dump(magento_shoppingCartPaymentList($carrinho, $store_id));
-//       // echo "<h1>INFORMAÇÕES DA LOJA-SHIPPING:</h1> <BR>";
-//       // var_dump(magento_shoppingCartshippingList($carrinho, $store_id));
-//
-//
-//   echo "Cart Shipping: ".magento_shoppingCartShippingMethod($carrinho,$store_id)."<br>";
-//   //var_dump($payment);
-//   echo "Dados de pagamento: ".magento_shoppingCartPaymentMethod($carrinho, $payment,$store_id)."<br>";
-//
-//
+ $prods = array(
+   array('EP-51-40983','1'),
+   array('EP-51-40654','4')
+ );
+
+ $billing =
+ array(
+   array(
+     'mode' => 'billing',
+     'firstname' => 'Teste Magento',
+     'lastname' => 'Teste Magento',
+     'street' => 'street address',
+     'city' => 'Ficticio de Teste',
+     'region' => 'SP',
+     'postcode' => 'postcode',
+     'country_id' => 'BR',
+     'telephone' => '34343434343434',
+     'is_default_billing' => 0,
+     'is_default_shipping' => 0),
+   array(
+     'mode' => 'shipping',
+     'firstname' => 'Teste Magento',
+     'lastname' => 'Teste Magento',
+     'street' => 'street address',
+     'city' => 'Ficticio de Teste',
+     'region' => 'SP',
+     'postcode' => 'postcode',
+     'country_id' => 'BR',
+     'telephone' => '34343434343434',
+     'is_default_billing' => 0,
+     'is_default_shipping' => 0)
+   );
+
+ // $customerCreate = array(
+ //   'email' => 'cust-tst-mail@example.org',
+ //   'firstname' => 'Dough',
+ //   'lastname' => 'Deeks',
+ //   'password' => 'password',
+ //   'website_id' => 1,
+ //   'store_id' => 1,
+ //   'group_id' => 1);
+ //
+ // var_dump(magento_customerCustomerCreate($customerCreate));
+
+ // $cust = array(
+ //   //concatenar o email no nome
+ //     'email' => 'customer-mail@example.org',
+ //     'firstname' => "testFirstname",
+ //     'lastname' => "testLastName",
+ //     'mode' => "guest",
+ //     'group_id' => "1"
+ //     );
+
+ $customer = array(
+   'firstname' => rand(1,100)."testFirstname",
+   'lastname' => "testLastName1",
+   'email' => rand(1,100)."testmailcustomer1@mail.com",
+   'telephone' => rand(1,100)."0001234567",
+   'taxvat' => rand(100000, 150000),
+   'group_id' => "1",
+   'store_id' => "21",
+   'website_id' => "2"
+ );
+
+ $id_customer = magento_customerCustomerCreate($customer);
+
+ $session = magento_session();
+ $obj_magento = magento_obj();
+ $obj_magento->customerAddressCreate($session, 34, array(
+ 'firstname' => rand(1,100).'teste1',
+ 'lastname' => 'testando',
+ 'street' => array('Street line 1', 'Streer line 2'),
+ 'city' => 'teste',
+ 'country_id' => 'BR',
+ 'region' => 'SP',
+ 'postcode' => rand(1,100).'96093',
+ 'telephone' => rand(1,100).'-623-2513',
+ 'is_default_billing' => FALSE,
+ 'is_default_shipping' => FALSE));
+
+ //var_dump($obj_magento);
+
+
+ $dados_customer = magento_customerCustomerList($id_customer);
+ var_dump($dados_customer);
+
+
+ $cust = array(
+     'customer_id' => $id_customer,
+     'mode' => "customer"
+    );
+
+ $payment = array(
+     'po_number' => null,
+     'method' => 'cashondelivery',
+     'cc_cid' => null,
+     'cc_owner' => null,
+     'cc_number' => null,
+     'cc_type' => null,
+     'cc_exp_year' => null,
+     'cc_exp_month' => null
+     );
+
+   // var_dump($prods);
+
+
+
+   global $store_id;
+
+   $carrinho = magento_shoppingCartCreate($store_id);
+
+ $id = magento_session();
+
+ // $obj_cust = magento_customerCustomerList($id);
+ // $obj_cust = (array) $obj_cust;
+
+ // var_dump($obj_cust);
+ //
+ // var_dump(magento_obj()->customerGroupList($id));
+ //
+
+   echo "Retorno cartProdAdd: <br>";
+   var_dump(magento_shoppingCartProductAdd($carrinho,$prods,$store_id));
+   echo "<br>";
+
+   echo "Lista shoppingCartProductList: <br>";
+   $lista_carrinho = magento_shoppingCartProductList($carrinho, $store_id);
+   var_dump($lista_carrinho);
+
+   echo "# de itens shoppingCartProductList: ".count($lista_carrinho)." <br>";
+   var_dump($cust);
+   echo "Customer Set: ".magento_shoppingCartCustomerSet($carrinho,$cust ,$store_id)." <br>";
+
+   var_dump($billing);
+   echo "Billing Set: ".magento_shoppingCartCustomerAddresses($carrinho, $billing,$store_id)." <br>";
+
+
+
+        echo "<h1>INFORMAÇÕES DA LOJA-PAYMENT:</h1> <BR>";
+        var_dump(magento_shoppingCartPaymentList($carrinho, $store_id));
+        echo "<h1>INFORMAÇÕES DA LOJA-SHIPPING:</h1> <BR>";
+        var_dump(magento_shoppingCartshippingList($carrinho, $store_id));
+
+
+
+   echo "Cart Shipping: ".magento_shoppingCartShippingMethod($carrinho,$store_id)."<br>";
+   //var_dump($payment);
+   echo "Dados de pagamento: ".magento_shoppingCartPaymentMethod($carrinho, $payment,$store_id)."<br>";
+
+
 // // var_dump(magento_shoppingCartInfo($carrinho));
 // $order_id = magento_shoppingCartOrder($carrinho,$store_id);
 // echo "Cart Order: ".$order_id."<br>";
 
 //
-//   //var_dump(magento_StoreList());
+   //var_dump(magento_StoreList());
 // $mod_email= array(
 //   'orderIncrementId' => $order_id,
 //   'status' => 'pending',
--- orderAdd.php
+++ orderAdd.php
@@ -9,13 +9,13 @@ class Magento_order
 public function Magento_order($dadosVenda)
 {
   $this->data = new stdClass();
-    $this->data->id_order[] = $dadosVenda->id_order;
-  $this->data->mlb_produto[] = $dadosVenda->mlb_produto;
-  $this->data->sku_produto[] = $dadosVenda->sku_produto;
-  $this->data->nome_produto[] = $dadosVenda->nome_produto;
-  $this->data->qtd_produto[] = $dadosVenda->qtd_produto;
-  $this->data->preco_unidade_produto[] =$dadosVenda->preco_unidade_produto;
-  $this->data->preco_total_produto[] = $dadosVenda->preco_total_produto;
+  $this->data->id_order = $dadosVenda->order_id;
+  $this->data->mlb_produto = $dadosVenda->mlb_produto;
+  $this->data->sku_produto = $dadosVenda->sku_produto;
+  $this->data->nome_produto = $dadosVenda->nome_produto;
+  $this->data->qtd_produto = $dadosVenda->qtd_produto;
+  $this->data->preco_unidade_produto =$dadosVenda->preco_unidade_produto;
+  $this->data->preco_total_produto = $dadosVenda->preco_total_produto;
 
   //--------------PAGAMENTO---------
   $this->data->id_meio_pagamento = $dadosVenda->id_meio_pagamento;
@@ -45,287 +45,239 @@ public function Magento_order($dadosVenda)
   $this->data->numero_documento_comprador = $dadosVenda->numero_documento_comprador;
 
 
-//// function magento_customerCustomerCreate()
-//// {
-//  global $magento_soap_user;
-//  global $magento_soap_password;
-//  global $store_id;
-//  global $DEBUG;
-//  global $shipping_method;
-//
-//  $obj_magento = magento_obj();
-//  $session = magento_session();
-//
-//  $customer = array(
-//    'firstname' => $this->data->nome_comprador,
-//    'lastname' => $this->data->sobrenome_comprador,
-//    'email' => $this->data->email_comprador,
-//    'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
-//    'taxvat' => $this->data->numero_documento_comprador,
-//    'group_id' => "1",
-//    'store_id' => "21",
-//    'website_id' => "2"
-//  );
-//
-//  $complexFilter = array(
-//    'complex_filter' => array(
-//        array(
-//            'key' => 'email',
-//            'value' => array('key' => 'in', 'value' => $customer['email'])
-//        )
-//    )
-//  );
-//
-//  $return = $obj_magento->customerCustomerList($session, $complexFilter);
-//
-//  if(!$return){
-//    $id_customer = $obj_magento->customerCustomerCreate($session, $customer);
-//    if($DEBUG == TRUE) var_dump($id_customer);
-//  }
-//  else
-//  {
-//    $id_customer = $return[0]->customer_id;
-//    if($DEBUG == TRUE) var_dump($id_customer);
-//    if($DEBUG == TRUE) var_dump($return);
-//  }
-//
-//// function magento_customerAddressCreate($id_customer)
-//// {
-//
-//
-//  $customer_address = array(
-//  'firstname' => $this->data->nome_comprador,
-//  'lastname' => $this->data->sobrenome_comprador,
-//  'street' => array($this->data->rua.", ".$this->data->numero." - ".$this->data->bairro,'' ),
-//  'city' => $this->data->cidade,
-//  'country_id' => $this->data->pais,
-//  'region' => $this->data->estado,
-//  'postcode' => $this->data->cep,
-//  'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
-//  'is_default_billing' => FALSE,
-//  'is_default_shipping' => FALSE);
-//
-//  $return = $obj_magento->customerAddressCreate($session, $id_customer, $customer_address);
-//
-//  if($DEBUG == TRUE) echo $return;
-//
-//
-//  $obj_mag = $obj_magento->customerAddressList($session, $id_customer);
-//
-//  if($DEBUG == TRUE) echo "<h1>Debug</h1>";var_dump($obj_mag);
-//
-//  $obj_mag_email = $obj_magento->customerCustomerInfo($session, $id_customer);
-//  $obj_mag = $obj_mag['0'];
-//
-//  if($DEBUG == TRUE) var_dump($obj_mag);
-//
-//  $name = $obj_mag->firstname." ".$obj_mag->lastname;
-//  $email = $obj_mag_email->email;
-//  $document = preg_replace('/\D/', '',$obj_mag_email->taxvat);
-//  $city = $obj_mag->city;
-//  $region = $obj_mag->region;
-//  $postcode = preg_replace('/\D/', '',$obj_mag->postcode);
-//  $street = $obj_mag->street;
-//  $phone = preg_replace('/\D/', '',$obj_mag->telephone);
-//
-//
-//  $return = array(
-//    'name' => $name,
-//    'email' => $email,
-//    'document' => $document,
-//    'city' => $city,
-//    'region' => $region,
-//    'postcode' => $postcode,
-//    'street' => $street,
-//    'phone' => $phone,
-//  );
-//
-//  if($DEBUG == true) var_dump($return);
-//// }
-//
-//// function magento_shoppingCartCreate($store_id)
-//// {
-////   global $magento_soap_user;
-//// 	global $magento_soap_password;
-////
-//// 	$obj_magento = magento_obj();
-//// 	$session = magento_session();
-//
-//	$cart_id = $obj_magento->shoppingCartCreate($session, $store_id);
-//
-//	if($DEBUG == TRUE) var_dump($cart_id);
-//// }
-//
-//// function magento_shoppingCartProductAdd($cart_id, $store_id)
-//// {
-//  //$prods Array template
-//	// array(
-//	// 	array($sku,$qty),
-//	// 	array($sku,$qty)
-//	// )
-//
-//	// global $magento_soap_user;
-//	// global $magento_soap_password;
-//
-//	$return = 0;
-//
-//	// $obj_magento = magento_obj();
-//	// $session = magento_session();
-//
-//  
-//
-//
-//	foreach ($Magento_order->sku_produto as $key => $value) {
-//          
-//        $shoppingCartProductEntity[$key] = array($Magento_order->sku_produto[$key],$Magento_order->qtd_produto[$key]);
-//        
-//        
-//}
-//		// var_dump($shoppingCartProductEntity); //DEBUG
-//
-//  
-//
-//  $result_prod_add = $obj_magento->shoppingCartProductAdd($session, $cart_id, $shoppingCartProductEntity, $store_id);
-//	if($result_prod_add) $return++;
-//	// var_dump($return);
-//
-//	if($DEBUG == TRUE) var_dump($return);
-//// }
-//
-//// function magento_shoppingCartProductList($cart_id, $store_id)
-//// {
-////   global $magento_soap_user;
-////   global $magento_soap_password;
-////
-////   $obj_magento = magento_obj();
-////   $session = magento_session();
-//
-//  $result = $obj_magento->shoppingCartProductList($session, $cart_id, $store_id);
-//
-//  	if($DEBUG == TRUE) var_dump($result);
-//// }
-//
-//// function magento_shoppingCartCustomerSet($cart_id, $id_customer, $store_id)
-//// {
-////   global $magento_soap_user;
-////   global $magento_soap_password;
-////
-////   $obj_magento = magento_obj();
-////   $session = magento_session();
-//
-//  $customer = array(
-//    'customer_id' => $id_customer,
-//    'mode' => "customer"
-//    );
-//
-//  	$return = $obj_magento->shoppingCartCustomerSet($session, $cart_id, $customer, $store_id);
-//
-//  	if($DEBUG == TRUE) echo $return;
-//// }
-//
-//// function magento_shoppingCartCustomerAddresses($cart_id, $store_id)
-//// {
-//// 	global $magento_soap_user;
-//// 	global $magento_soap_password;
-////
-//// 	$obj_magento = magento_obj();
-//// 	$session = magento_session();
-//
-//  $billing =
-//  array(
-//    array(
-//      'mode' => 'billing',
-//      'firstname' => $this->data->nome_comprador,
-//      'lastname' => $this->data->sobrenome_comprador,
-//      'street' => $this->data->rua.", ".$this->data->numero." - ".$this->data->bairro,
-//      'city' => $this->data->cidade,
-//      'region' => $this->data->estado,
-//      'postcode' => $this->data->cep,
-//      'country_id' => $this->data->pais,
-//      'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
-//      'is_default_billing' => 0,
-//      'is_default_shipping' => 0),
-//    array(
-//      'mode' => 'shipping',
-//      'firstname' => $this->data->nome_comprador,
-//      'lastname' => $this->data->sobrenome_comprador,
-//      'street' => $this->data->rua.", ".$this->data->numero."-".$this->data->bairro,
-//      'city' => $this->data->cidade,
-//      'region' => $this->data->estado,
-//      'postcode' => $this->data->cep,
-//      'country_id' => $this->data->pais,
-//      'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
-//      'is_default_billing' => 0,
-//      'is_default_shipping' => 0)
-//    );
-//
-//	$return = $obj_magento->shoppingCartCustomerAddresses($session, $cart_id, $billing, $store_id);
-//
-//if($DEBUG == TRUE) var_dump($return);
-//// }
-//
-//// function magento_shoppingCartShippingMethod($cart_id, $store_id)
-//// {
-//// 	global $magento_soap_user;
-//// 	global $magento_soap_password;
-////   global $shipping_method;
-////
-//// 	$obj_magento = magento_obj();
-//// 	$session = magento_session();
-//
-//	$return =  $obj_magento->shoppingCartShippingMethod($session, $cart_id, $shipping_method, $store_id);
-//
-//	if($DEBUG == TRUE) var_dump($return);
-//// }
-////
-//// function magento_shoppingCartPaymentMethod($cart_id, $store_id){
-//// 	global $magento_soap_user;
-//// 	global $magento_soap_password;
-////
-//// 	$obj_magento = magento_obj();
-//// 	$session = magento_session();
-//
-//  $payment = array(
-//      'po_number' => null,
-//      'method' => 'cashondelivery',
-//      'cc_cid' => null,
-//      'cc_owner' => null,
-//      'cc_number' => null,
-//      'cc_type' => null,
-//      'cc_exp_year' => null,
-//      'cc_exp_month' => null
-//      );
-//
-//	$return =  $obj_magento->shoppingCartPaymentMethod($session, $cart_id, $payment, $store_id);
-//
-//	if($DEBUG == TRUE) var_dump($return);
-//// }
-////
-//// function magento_shoppingCartOrder($cart_id, $store_id)
-//// {
-//// 	global $magento_soap_user;
-//// 	global $magento_soap_password;
-////
-//// 	$obj_magento = magento_obj();
-//// 	$session = magento_session();
-//
-//	$order_id =  $obj_magento->shoppingCartOrder($session, $cart_id, $store_id);
-//
-//if($DEBUG == TRUE) var_dump($order_id);
-//// }
-////
-//// function magento_salesOrderAddComment($order_id, $status, $comment)
-//// {
-//// 	global $magento_soap_user;
-//// 	global $magento_soap_password;
-////
-//// 	$obj_magento = magento_obj();
-//// 	$session = magento_session();
-//
-//  $comment = 'Id do Pedido MLB: '.$this->data->id_order;
-//
-//	$return = $obj_magento->salesOrderAddComment($session, $order_id, 'pending', $comment, null);
-//
-//if($DEBUG == TRUE) var_dump($return);
+// function magento_customerCustomerCreate()
+// {
+  global $magento_soap_user;
+  global $magento_soap_password;
+  global $store_id;
+  global $DEBUG;
+  global $shipping_method;
+
+  $obj_magento = magento_obj();
+  $session = magento_session();
+
+  $customer = array(
+    'firstname' => $this->data->nome_comprador,
+    'lastname' => $this->data->sobrenome_comprador,
+    'email' => $this->data->email_comprador,
+    'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
+    'taxvat' => $this->data->numero_documento_comprador,
+    'group_id' => "1",
+    'store_id' => "21",
+    'website_id' => "2"
+  );
+
+  $complexFilter = array(
+    'complex_filter' => array(
+        array(
+            'key' => 'email',
+            'value' => array('key' => 'in', 'value' => $customer['email'])
+        )
+    )
+  );
+
+  $return = $obj_magento->customerCustomerList($session, $complexFilter);
+
+  if(!$return){
+    $id_customer = $obj_magento->customerCustomerCreate($session, $customer);
+    if($DEBUG == TRUE) echo "<h1>id Customer</h1>";var_dump($id_customer);
+  }
+  else
+  {
+    $id_customer = $return[0]->customer_id;
+    if($DEBUG == TRUE) var_dump($id_customer);
+    if($DEBUG == TRUE) var_dump($return);
+  }
+
+// function magento_customerAddressCreate($id_customer)
+
+  $customer_address = array(
+  'firstname' => $this->data->nome_comprador,
+  'lastname' => $this->data->sobrenome_comprador,
+  'street' => array($this->data->rua.", ".$this->data->numero." - ".$this->data->bairro,'' ),
+  'city' => $this->data->cidade,
+  'country_id' => $this->data->pais,
+  'region' => $this->data->estado,
+  'postcode' => $this->data->cep,
+  'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
+  'is_default_billing' => FALSE,
+  'is_default_shipping' => FALSE);
+
+    var_dump($customer_address);
+  $return = $obj_magento->customerAddressCreate($session, $id_customer, $customer_address);
+
+  if($DEBUG == TRUE) echo "AddressesCreate".$return;
+
+
+  $obj_mag = $obj_magento->customerAddressList($session, $id_customer);
+
+  if($DEBUG == TRUE) echo "<h1>addressesList</h1>";var_dump($obj_mag);
+
+  $obj_mag_email = $obj_magento->customerCustomerInfo($session, $id_customer);
+  $obj_mag = $obj_mag['0'];
+
+  if($DEBUG == TRUE) echo "<h1>CustomerInfo</h1>";var_dump($obj_mag);
+
+  $name = $obj_mag->firstname." ".$obj_mag->lastname;
+  $email = $obj_mag_email->email;
+  $document = preg_replace('/\D/', '',$obj_mag_email->taxvat);
+  $city = $obj_mag->city;
+  $region = $obj_mag->region;
+  $postcode = preg_replace('/\D/', '',$obj_mag->postcode);
+  $street = $obj_mag->street;
+  $phone = preg_replace('/\D/', '',$obj_mag->telephone);
+
+
+  $return = array(
+    'name' => $name,
+    'email' => $email,
+    'document' => $document,
+    'city' => $city,
+    'region' => $region,
+    'postcode' => $postcode,
+    'street' => $street,
+    'phone' => $phone,
+  );
+
+  if($DEBUG == true) echo "<h1>Array Customer</h1>";var_dump($return);
+    
+   
+    
+// function magento_shoppingCartCreate($store_id)
+
+	$cart_id = $obj_magento->shoppingCartCreate($session, $store_id);
+
+	if($DEBUG == TRUE) echo "<h1>shoppingCartCreate</h1>";var_dump($cart_id);
+
+    $return = $obj_magento->storeInfo($session, $store_id);
+              
+    echo "storeInfo Ativos: ";
+    var_dump($return);
+    
+       
+// function magento_shoppingCartProductAdd($cart_id, $store_id)
+
+	$return = 0;
+
+	foreach ($this->data->sku_produto as $key => $value) {
+          
+        $shoppingCartProductEntity[$key] = array($this->data->sku_produto[$key],$this->data->qtd_produto[$key]);
+        
+        
+}
+		if($DEBUG == TRUE) echo "<h1>shoppingCartEntity</h1>";var_dump($shoppingCartProductEntity); //DEBUG
+
+  
+
+  $result_prod_add = $obj_magento->shoppingCartProductAdd($session, $cart_id, $shoppingCartProductEntity, $store_id);
+	if($result_prod_add) $return++;
+
+	if($DEBUG == TRUE) echo "<h1>shoppingCartProductAdd</h1>";var_dump($return);
+
+
+    
+    
+// function magento_shoppingCartProductList($cart_id, $store_id)
+
+  $result = $obj_magento->shoppingCartProductList($session, $cart_id, $store_id);
+
+  	if($DEBUG == TRUE) echo "<h1>shoppingCartProductList</h1>";var_dump($result);
+
+
+    
+    
+// function magento_shoppingCartCustomerSet($cart_id, $id_customer, $store_id)
+
+  $customer = array(
+    'customer_id' => $id_customer,
+    'mode' => "customer"
+    );
+
+  	$return = $obj_magento->shoppingCartCustomerSet($session, $cart_id, $customer, $store_id);
+
+  	if($DEBUG == TRUE) echo "CartCustomerSet".$return;
+
+
+    
+    
+    
+// function magento_shoppingCartCustomerAddresses($cart_id, $store_id)
+
+  $billing =
+  array(
+    array(
+      'mode' => 'billing',
+      'firstname' => $this->data->nome_comprador,
+      'lastname' => $this->data->sobrenome_comprador,
+      'street' => $this->data->rua.", ".$this->data->numero." - ".$this->data->bairro,
+      'city' => $this->data->cidade,
+      'region' => $this->data->estado,
+      'postcode' => $this->data->cep,
+      'country_id' => $this->data->pais,
+      'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
+      'is_default_billing' => 0,
+      'is_default_shipping' => 0),
+    array(
+      'mode' => 'shipping',
+      'firstname' => $this->data->nome_comprador,
+      'lastname' => $this->data->sobrenome_comprador,
+      'street' => $this->data->rua.", ".$this->data->numero."-".$this->data->bairro,
+      'city' => $this->data->cidade,
+      'region' => $this->data->estado,
+      'postcode' => $this->data->cep,
+      'country_id' => $this->data->pais,
+      'telephone' => $this->data->cod_area_comprador.$this->data->telefone_comprador,
+      'is_default_billing' => 0,
+      'is_default_shipping' => 0)
+    );
+
+	$return = $obj_magento->shoppingCartCustomerAddresses($session, $cart_id, $billing, $store_id);
+
+if($DEBUG == TRUE) echo "<h1>shoppingCartCustomerAddresses</h1>";var_dump($return);
+
+
+        echo "<h1>INFORMAÇÕES DA LOJA-SHIPPING:</h1> <BR>";
+        var_dump($obj_magento->shoppingCartShippingList($session, $cart_id, $store_id));
+    
+// function magento_shoppingCartShippingMethod($cart_id, $store_id)
+
+	$return =  $obj_magento->shoppingCartShippingMethod($session, $cart_id, $shipping_method, $store_id);
+
+	if($DEBUG == TRUE) echo "<h1>shoppingCartShippingMethod</h1>";var_dump($return);
+
+  $payment = array(
+      'po_number' => null,
+     'method' => 'cashondelivery',
+     'cc_cid' => null,
+     'cc_owner' => null,
+     'cc_number' => null,
+     'cc_type' => null,
+     'cc_exp_year' => null,
+     'cc_exp_month' => null
+      );
+
+	$return =  $obj_magento->shoppingCartPaymentMethod($session, $cart_id, $payment, $store_id);
+
+	if($DEBUG == TRUE) echo "<h1>ShoppingCartPaymentMetod</h1>";var_dump($return);
+
+        
+// function magento_shoppingCartOrder($cart_id, $store_id)
+
+	$order_id =  $obj_magento->shoppingCartOrder($session, $cart_id, $store_id);
+
+if($DEBUG == TRUE) echo "<h1>shoppingCartOrder</h1>";var_dump($order_id);
+
+
+        echo "<h1>INFORMAÇÕES DA LOJA-PAYMENT:</h1> <BR>";
+        var_dump($obj_magento->shoppingCartPaymentList($session, $cart_id, $store_id));
+    
+    
+// function magento_salesOrderAddComment($order_id, $status, $comment)
+
+  $comment = 'Id do Pedido MLB: '.$this->data->id_order;
+
+	$return = $obj_magento->salesOrderAddComment($session, $order_id, 'pending', $comment, null);
+
+if($DEBUG == TRUE) echo "<h1>salesOrderAddComment</h1>";var_dump($return);
 }
 }
