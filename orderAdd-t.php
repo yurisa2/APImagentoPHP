e5af02e78b2a5710e603e549bd634b0532e8e4f7
luigimuzy <luigifracalanza@gmail.com>
2018-07-10 16:14:12 -0300

    atualização dos arquivos

--- orderAdd-t.php
+++ orderAdd-t.php
@@ -1,12 +1,13 @@
 <?php
 
-class Magento_order{
+class Magento_orders{
   /**
   * Construtor. Set properties in Magento_order
   * @param object $dadosVenda;
   */
-  public function Magento_order($dadosVenda)
+  public function Magento_orders($dadosVenda)
   {
+
     $this->data = new stdClass();
     $this->data->id_order = $dadosVenda->order_id;
     $this->data->mlb_produto = $dadosVenda->mlb_produto;
@@ -42,15 +43,17 @@ class Magento_order{
     $this->data->sobrenome_comprador = $dadosVenda->sobrenome_comprador;
     $this->data->tipo_documento_comprador = $dadosVenda->tipo_documento_comprador;
     $this->data->numero_documento_comprador = $dadosVenda->numero_documento_comprador;
+  }
 
-    global $magento_soap_user;
-    global $magento_soap_password;
-    global $store_id;
-    global $DEBUG;
-    global $shipping_method;
+    protected $magento_soap_user;
+    protected $magento_soap_password;
+    protected $store_id;
+    protected $shipping_method;
 
-    $obj_magento = magento_obj();
-    $session = magento_session();
+public function CustomerCreate()
+{
+  $obj_magento = magento_obj();
+  $session = magento_session();
 
     $customer = array(
       'firstname' => $this->data->nome_comprador,
@@ -112,8 +115,12 @@ class Magento_order{
       if($DEBUG == TRUE) echo "<h1>Customer</h1>";
       if($DEBUG == TRUE) var_dump($id_customer);
     }
+}
+public function AddressCreate($id_customer)
+{
+  $obj_magento = magento_obj();
+  $session = magento_session();
 
-// function magento_customerAddressCreate($id_customer)
     $obj_mag = $obj_magento->customerAddressList($session, $id_customer);
 
     if($DEBUG == TRUE) {echo "<h1>addressesList</h1>";var_dump($obj_mag);}
@@ -148,14 +155,23 @@ class Magento_order{
     );
 
     if($DEBUG == true){ echo "<h1>Array Customer</h1>";var_dump($return);}
+}
 
-// function magento_shoppingCartCreate($store_id)
+public function CartCreate($store_id){
+  $obj_magento = magento_obj();
+	$session = magento_session();
+
     $cart_id = $obj_magento->shoppingCartCreate($session, $store_id);
 
     echo "<br/>ID do Carrinho de Compras: ".$cart_id;
     if($DEBUG == TRUE) {echo "<h1>shoppingCartCreate</h1>";var_dump($cart_id);}
+}
 
-// function magento_shoppingCartProductAdd($cart_id, $store_id)
+public function ProductAdd($cart_id, $store_id)
+{
+  $obj_magento = magento_obj();
+	$session = magento_session();
+
     foreach ($this->data->sku_produto as $key => $value)
     {
       $shoppingCartProductEntity[$key] = array(
@@ -175,16 +191,25 @@ class Magento_order{
       //MANDAR EMAIL
       echo "<br/>Produtos não puderam ser adicionados".var_dump($result_prod_add);
     }
-// function magento_shoppingCartProductList($cart_id, $store_id)
-    $result = $obj_magento->shoppingCartProductList($session, $cart_id, $store_id);
+  }
+
+public function ProductList($cart_id, $store_id)
+{
+  $obj_magento = magento_obj();
+	$session = magento_session();
+  $result = $obj_magento->shoppingCartProductList($session, $cart_id, $store_id);
     if($DEBUG == TRUE)
     {
       echo "<h1>Produtos adicionados no carrinho: </h1>";
       var_dump($result);
     }
+}
 
-// function magento_shoppingCartCustomerSet($cart_id, $id_customer, $store_id)
-    $customer = array(
+public function CustomerSet($cart_id, $id_customer, $store_id)
+{
+  $obj_magento = magento_obj();
+	$session = magento_session();
+  $customer = array(
       'customer_id' => $id_customer,
       'mode' => "customer"
     );
@@ -201,9 +226,13 @@ class Magento_order{
       echo "<br/>Não foi possível Setar Customer";
     }
     if($DEBUG == TRUE) echo "<h1>CartCustomerSet: ".$return."</h1>";
+}
 
-// function magento_shoppingCartCustomerAddresses($cart_id, $store_id)
-    $billing = array(
+public function CustomerAddresses($cart_id, $store_id)
+{
+  $obj_magento = magento_obj();
+	$session = magento_session();
+  $billing = array(
       array(
         'mode' => 'billing',
         'firstname' => $this->data->nome_comprador,
@@ -236,8 +265,12 @@ class Magento_order{
       else //Mandar email do erro
 
       if($DEBUG == TRUE) var_dump($return);
+}
 
-// function magento_shoppingCartShippingMethod($cart_id, $store_id)
+public function ShippingMethod($cart_id, $store_id)
+{
+  $obj_magento = magento_obj();
+	$session = magento_session();
       $return =  $obj_magento->shoppingCartShippingMethod($session, $cart_id, $shipping_method, $store_id);
 
       if ($return) echo "<br/>Setado Shipping Method para o carrinho";
@@ -248,6 +281,12 @@ class Magento_order{
         echo "<h1>shoppingCartShippingMethod</h1>";
         var_dump($return);
       }
+}
+
+public function PaymentMethod($cart_id, $store_id)
+{
+  $obj_magento = magento_obj();
+  $session = magento_session();
 
       $payment = array(
         'po_number' => null,
@@ -266,16 +305,24 @@ class Magento_order{
       else //Mandar email do erro
 
       if($DEBUG == TRUE) {echo "<h1>ShoppingCartPaymentMetod</h1>";var_dump($return);}
+}
 
-//function magento_shoppingCartOrder($cart_id, $store_id)
+public function CartOrder($cart_id, $store_id)
+{
+  $obj_magento = magento_obj();
+	$session = magento_session();
     $order_id = $obj_magento->shoppingCartOrder($session, $cart_id, $store_id);
 
     if($response["httpCode"] == 200) echo "Order criado - ".$order_id;
     else //mandar email;
 
     if($DEBUG == TRUE) {echo "<h1>shoppingCartOrder</h1>";var_dump($order_id);}
+}
 
-//function magento_salesOrderAddComment($order_id, $status, $comment)
+public function AddComment($order_id, $status, $comment)
+{
+  $obj_magento = magento_obj();
+	$session = magento_session();
     $comment="";
     foreach ($this->data->id_order as $key =>$value)
     {
