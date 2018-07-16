6037d286b8d58ecba37c9e2372a58eb1edd0ea14
luigimuzy <luigifracalanza@gmail.com>
2018-07-16 19:14:18 -0300

    adicionado as variaveis globais shipping_method e store_id

--- include/config.php
+++ include/config.php
@@ -2,9 +2,12 @@
 global $wsdl;
 global $magento_soap_user;
 global $magento_soap_password;
+global $shipping_method;
+global $store_id;
 
 $wsdl = 'http://easypath.com.br/api/v2_soap?wsdl=1';  //Put here your DOMAIN
 $magento_soap_user = 'yurisa2_ml';                                  // SOAP-USER
 $magento_soap_password = '999999';                              // PASSWORD  (SOAP USER)
-
- ?>
+$shipping_method = "freeshipping_freeshipping";         //METODO DE ENVIO PADRAO DO MAGENTO
+$store_id = "21";                           //id da loja 
+?>
